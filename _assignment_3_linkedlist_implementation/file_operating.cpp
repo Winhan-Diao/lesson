8a043c578067fa572167c19247eb6fa08ed3ab6a
WINHAN <3294697230@qq.com>
2024-09-11 13:25:52 +0800

    Improve pattern

--- _assignment_3_linkedlist_implementation/file_operating.cpp
+++ _assignment_3_linkedlist_implementation/file_operating.cpp
@@ -5,47 +5,41 @@ void createFolder(const std::string& folderName) {
         throw std::runtime_error{"Failed to create folder."};
 }
 
-// both paths shall exist
-void copyFiles(std::string sourcePath, std::string destPath, std::string wildCardMatching) {
-    if (sourcePath.back() != '\\' && sourcePath.back() != '/')
-        sourcePath += '/';
-    if (destPath.back() != '\\' && destPath.back() != '/')
-        destPath += '/';
-    std::string sourcePathMatchingFiles = sourcePath + wildCardMatching;
-    std::cout << "[Copy] Match case: " << sourcePathMatchingFiles << "\r\n";     //debug
-    WIN32_FIND_DATAA findFileDataA;
-    HANDLE hFind = FindFirstFileA(sourcePathMatchingFiles.c_str(), &findFileDataA);
-    if (hFind == INVALID_HANDLE_VALUE) {
-        throw std::runtime_error{"Fail to find any file at "s + sourcePath + "; with Windows error code "s + std::to_string(GetLastError())};
-        return;
+void AbstractFilesTraversing::operator() () {
+    HANDLE hfind = FindFirstFileA((traversingPath + wildcardMatching).c_str(), &findFileDataA);
+    if (hfind == INVALID_HANDLE_VALUE) {
+        switch (GetLastError()) {
+            case 0x2: 
+                throw std::runtime_error{"No file is found in the directory with wildcard matching case: "s + traversingPath + wildcardMatching + "; with Windows error code "s + std::to_string(GetLastError())};
+                break;
+            case 0x3:
+                throw std::runtime_error{"The path with wildcard mathcing case is invalid: "s + traversingPath + wildcardMatching + "; with Windows error code "s + std::to_string(GetLastError())};
+                break;
+            default: 
+                throw std::runtime_error{"Fail to find any file at "s + traversingPath + wildcardMatching + "; with Windows error code "s + std::to_string(GetLastError())};
+        }
     }
     do {
-        if (!(findFileDataA.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-            if (!CopyFileA((sourcePath + findFileDataA.cFileName).c_str(), (destPath + findFileDataA.cFileName).c_str(), FALSE))
-                throw std::runtime_error{"Failed to copy a file"};
-            else
-                std::cout << "[Copy] Copied to: " << (destPath + findFileDataA.cFileName) << "\r\n";        //debug
-        }
-    } while (FindNextFileA(hFind, &findFileDataA));
-    FindClose(hFind);
+        if (!(findFileDataA.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+            fileOperating();
+        else 
+            dirOperating();
+    } while (FindNextFileA(hfind, &findFileDataA));
+    FindClose(hfind);
 }
 
-void deleteFiles(std::string targetPath, std::string wildCardMatching) {
-    if (targetPath.back() != '\\' && targetPath.back() != '/')
-        targetPath += '/';
-    WIN32_FIND_DATAA findFileDataA;
-    HANDLE hFind = FindFirstFileA((targetPath + wildCardMatching).c_str(), &findFileDataA);
-    if (hFind == INVALID_HANDLE_VALUE) {
-        throw std::runtime_error{"Fail to find any file at "s + targetPath + "; with Windows error code "s + std::to_string(GetLastError())};
-        return;
-    }
-    do {
-        if (!(findFileDataA.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-            if (!DeleteFileA((targetPath + findFileDataA.cFileName).c_str()))
-                throw std::runtime_error{"Failed to delete a file"};
-            else 
-                std::cout << "[Delete] Deleted: " << (targetPath + findFileDataA.cFileName) << "\r\n";        //debug
-        }
-    } while (FindNextFileA(hFind, &findFileDataA));
-    FindClose(hFind);
+void CopyFilesTraversing::fileOperating() {
+    std::cout << "helloworld from CopyFilesTraversing::fileOperating" << "\r\n";        //debug
+    if (!CopyFileA((traversingPath + findFileDataA.cFileName).c_str(), (destPath + findFileDataA.cFileName).c_str(), FALSE))
+        throw std::runtime_error{"Failed to copy a file"};
+    else
+        std::cout << "[Copy] Copied to: " << (destPath + findFileDataA.cFileName) << "\r\n";        //debug
+}
+
+void DeleteFilesTraversing::fileOperating() {
+    std::cout << "helloworld from DeleteFilesTraversing::fileOperating" << "\r\n";        //debug
+    if (!DeleteFileA((traversingPath + findFileDataA.cFileName).c_str()))
+        throw std::runtime_error{"Failed to delete a file"};
+    else 
+        std::cout << "[Delete] Deleted: " << (traversingPath + findFileDataA.cFileName) << "\r\n";        //debug
 }
--- _assignment_3_linkedlist_implementation/file_operating.h
+++ _assignment_3_linkedlist_implementation/file_operating.h
@@ -24,6 +24,14 @@ inline void toPath(std::string& dirOrPath);
 6. [x] Delete files
  */
 
+void createFolder(const std::string& folderName);
+
+#define copyFiles(sourcePath, destPath, ...) CopyFilesTraversing(sourcePath, destPath, ##__VA_ARGS__)()
+
+#define deleteFiles(traversingPath, ...) DeleteFilesTraversing(traversingPath, ##__VA_ARGS__)()
+
+#define writeHashInfo(hashInfoConsumer, traversingPath, ...) WriteHashInfoFilesTraversing(hashInfoConsumer, traversingPath, ##__VA_ARGS__)()
+
 struct AbstractFilesTraversing {
 private:
 protected:
@@ -37,86 +45,47 @@ public:
     AbstractFilesTraversing(T1&& traversingPath, T2&& wildcardMatching)
         : traversingPath{std::forward<T1>(traversingPath)}
         , wildcardMatching{std::forward<T2>(wildcardMatching)} 
-    {}
-    void operator() () {
-        toPath(traversingPath);
-        HANDLE hfind = FindFirstFileA((traversingPath + wildcardMatching).c_str(), &findFileDataA);
-        if (hfind == INVALID_HANDLE_VALUE) {
-            switch (GetLastError()) {
-                case 0x2: 
-                    throw std::runtime_error{"No file is found in the directory with wildcard matching case: "s + traversingPath + wildcardMatching + "; with Windows error code "s + std::to_string(GetLastError())};
-                    break;
-                case 0x3:
-                    throw std::runtime_error{"The path with wildcard mathcing case is invalid: "s + traversingPath + wildcardMatching + "; with Windows error code "s + std::to_string(GetLastError())};
-                    break;
-                default: 
-                    throw std::runtime_error{"Fail to find any file at "s + traversingPath + wildcardMatching + "; with Windows error code "s + std::to_string(GetLastError())};
-            }
-        }
-        do {
-            if (!(findFileDataA.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
-                fileOperating();
-            else 
-                dirOperating();
-        } while (FindNextFileA(hfind, &findFileDataA));
-        FindClose(hfind);
+    {
+        toPath(this->traversingPath);
     }
+    void operator() ();
     virtual ~AbstractFilesTraversing() noexcept = default;
 };
 
 struct CopyFilesTraversing: public AbstractFilesTraversing{
 private:
     std::string destPath;
+    void fileOperating() override;
 public:
-    template<class T1, class T2, class T3, typename = typename std::enable_if<all_convertible<std::string, T1, T2, T3>::value>>
+    template<class T1, class T2, class T3 = decltype("*"s), typename = typename std::enable_if<all_convertible<std::string, T1, T2, T3>::value>>
     CopyFilesTraversing(T1&& sourcePath, T2&& destPath, T3&& wildCardMatching = "*"s)
         : AbstractFilesTraversing(std::forward<T1>(sourcePath), std::forward<T3>(wildCardMatching))
         , destPath(std::forward<T2>(destPath)) 
     {
         toPath(this->destPath);
     }
-    void fileOperating() override {
-        std::cout << "helloworld from CopyFilesTraversing::fileOperating" << "\r\n";        //debug
-        if (!CopyFileA((traversingPath + findFileDataA.cFileName).c_str(), (destPath + findFileDataA.cFileName).c_str(), FALSE))
-            throw std::runtime_error{"Failed to copy a file"};
-        else
-            std::cout << "[Copy] Copied to: " << (destPath + findFileDataA.cFileName) << "\r\n";        //debug
-    }
 };
 
 struct DeleteFilesTraversing: public AbstractFilesTraversing {
-    template<class T1, class T2, typename = typename std::enable_if<all_convertible<std::string, T1, T2>::value>>
+private:
+    void fileOperating() override;
+public:
+    template<class T1, class T2 = decltype("*"s), typename = typename std::enable_if<all_convertible<std::string, T1, T2>::value>>
     DeleteFilesTraversing(T1&& traversingPath, T2&& wildCardMatching = "*"s): AbstractFilesTraversing(std::forward<T1>(traversingPath), std::forward<T2>(wildCardMatching)) {}
-    void fileOperating() override {
-        std::cout << "helloworld from DeleteFilesTraversing::fileOperating" << "\r\n";        //debug
-        if (!DeleteFileA((traversingPath + findFileDataA.cFileName).c_str()))
-            throw std::runtime_error{"Failed to delete a file"};
-        else 
-            std::cout << "[Delete] Deleted: " << (traversingPath + findFileDataA.cFileName) << "\r\n";        //debug
-    }
 };
 
+template <class F>
 struct WriteHashInfoFilesTraversing: public AbstractFilesTraversing {
-    template<class T1, class T2, typename = typename std::enable_if<all_convertible<std::string, T1, T2>::value>>
-    WriteHashInfoFilesTraversing(T1&& traversingPath, T2&& wildCardMatching = "*"s): AbstractFilesTraversing(std::forward<T1>(traversingPath), std::forward<T2>(wildCardMatching)) {}
-    void fileOperating() override {
-        std::cout << "helloworld from DeleteFilesTraversing::fileOperating" << "\r\n";        //debug
-        if (!DeleteFileA((traversingPath + findFileDataA.cFileName).c_str()))
-            throw std::runtime_error{"Failed to delete a file"};
-        else 
-            std::cout << "[Delete] Deleted: " << (traversingPath + findFileDataA.cFileName) << "\r\n";        //debug
-    }
+private:
+    F hashInfoConsumer;
+    void fileOperating();
+public:
+    template <class T1, class T2 = decltype("*"s), typename = typename std::enable_if<all_convertible<std::string, T1, T2>::value && can_take<F, std::string&&, std::size_t>::value>>
+    WriteHashInfoFilesTraversing(F&& hashInfoConsumer, T1&& traversingPath, T2&& wildCardMatching = "*"s)
+        : AbstractFilesTraversing(std::forward<T1>(traversingPath), std::forward<T2>(wildCardMatching)) 
+        , hashInfoConsumer(std::forward<F>(hashInfoConsumer))
+    {}
 };
-
-void createFolder(const std::string& folderName);
-
-// both paths shall exist
-void copyFiles(std::string sourcePath, std::string destPath, std::string wildCardMatching = "*"s);
-
-void deleteFiles(std::string targetPath, std::string wildCardMatching = "*"s);
-
-template <class F, typename = std::enable_if_t<can_take<F, std::string&&, std::size_t>::value>>
-void readHashInfo(F&& hashInfoConsumer, std::string targetPath, std::string wildCardMatching = "*"s);
 
 // ==============================
 //         Definition
@@ -135,30 +104,17 @@ inline void toPath(std::string& dirOrPath) {
         dirOrPath = std::move(dirOrPath) + '/';
 }
 
-template <class F, typename>
-void readHashInfo(F&& hashInfoConsumer, std::string targetPath, std::string wildCardMatching) {
-    if (targetPath.back() != '\\' && targetPath.back() != '/')
-        targetPath += '/';
-    WIN32_FIND_DATAA findFileDataA;
-    HANDLE hFind = FindFirstFileA((targetPath + wildCardMatching).c_str(), &findFileDataA);
-    if (hFind == INVALID_HANDLE_VALUE) {
-        throw std::runtime_error{"Fail to find any file at "s + targetPath + "; with Windows error code "s + std::to_string(GetLastError())};
-        return;
+template <class F>
+void WriteHashInfoFilesTraversing<F>::fileOperating() {
+    std::cout << "helloworld from WriteHashInfoFilesTraversing::fileOperating" << "\r\n";        //debug
+    std::cout << "findFileDataA.cFileName: " << findFileDataA.cFileName << "\r\n";      //debug
+    if (std::ifstream ifs{traversingPath + findFileDataA.cFileName, std::ios::binary | std::ios::ate}) {
+        size_t size = ifs.tellg();
+        ifs.seekg(0);
+        std::string buffer(size, '\0');
+        ifs.read(&buffer[0], size);
+        hashInfoConsumer(std::move(traversingPath + findFileDataA.cFileName), std::hash<std::string>{}(buffer));
+    } else {
+        throw std::runtime_error{"std::ifstream failed to load a file"};
     }
-    std::string fileNameWithAbsPath;
-    do {
-        if (!(findFileDataA.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-            fileNameWithAbsPath = targetPath + findFileDataA.cFileName;
-            if (std::ifstream ifs{fileNameWithAbsPath, std::ios::binary | std::ios::ate}) {
-                size_t size = ifs.tellg();
-                ifs.seekg(0);
-                std::string buffer(size, '\0');
-                ifs.read(&buffer[0], size);
-                hashInfoConsumer(std::move(fileNameWithAbsPath), std::hash<std::string>{}(buffer));
-            } else {
-                throw std::runtime_error{"std::ifstream failed to load a file"};
-            }
-        }
-    } while (FindNextFileA(hFind, &findFileDataA));
-    FindClose(hFind);
 }
